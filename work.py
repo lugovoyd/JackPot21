a5abccdec5301b2d944f92beafb7d78fdd30a654
lugovoyd <60981398+lugovoyd@users.noreply.github.com>
2020-03-10 10:04:13 +0200

    Add files via upload

--- work.py
+++ work.py
@@ -2,12 +2,13 @@ koloda = [6,7,8,9,10,2,3,4,11] * 4
 import random
 random.shuffle(koloda)
 
+
 print('Play games?')
 count = 0
 
 while True:
-    choice = input('One more? 1/2_\n')
-    if choice=='1':
+    choice = input('One more? y/n \n')
+    if choice=='y':
         print('You have %d') 
         current = koloda.pop()
         print('You have %d' %current)
@@ -20,7 +21,7 @@ while True:
             break
         else:
             print('You have %d.' %count)
-    elif choice == '2':
+    elif choice == 'n':
         print('You have  %d and you end game.' %count)
         break
 
